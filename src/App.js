394cbb9f6696d23c0756ed03e30ae0811d86c32c
dinareda <dina.reda.elsebaey@gmail.com>
2022-04-19 04:46:47 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import NotFound from './pages/NotFound';
@@ -7,17 +6,16 @@ import ProductDetails from './pages/ProductDetails'
 
 
 import React from "react";
-import {
-  BrowserRouter,Route,Switch} from "react-router-dom";
+import { BrowserRouter, Route , Routes } from 'react-router-dom';
 function App() {
   return (
     <BrowserRouter>
     <Navbar/>
-    <Switch>
-      <Route path={'/'} exact component={Product}/>
-      <Route path={'/product-details/:id'}  component={ProductDetails}/>
-      <Route path={'*'} exact component={NotFound}/>
-    </Switch>
+    <Routes>
+      <Route path='/' element={<Product/>}/>
+      <Route path='/product-details/:id'  element={<ProductDetails/>}/>
+      <Route path='*' element={<NotFound/>}/>
+      </Routes>
     </BrowserRouter>
   );
 }
